ce87dd95690e1873622c97f0d8753cc91d8d6fae
paintingofblue <micgar19@kingshigh.school.nz>
2022-10-12 22:19:40 +1300

    Rewrote UI, got rid of server and channel selector
    Need to re-add message sending now

--- main.js
+++ main.js
@@ -1,108 +1,85 @@
-// basic vars
-var fs = require("fs"),
-  ini = require("ini");
+var blessed = require('blessed')
+, contrib = require('blessed-contrib')
+, screen = blessed.screen({
+    smartCSR: true,
+    fullUnicode: false,
+    dockBorders: true,
+    grabKeys: true
+})
+
+var ini = require("ini");
+var fs = require("fs");
 var config = ini.parse(fs.readFileSync("./config.ini", "utf-8"));
 var zip = (rows) => rows[0].map((_, c) => rows.map((row) => row[c]));
 
-// UI
-var CLI = require("clui"),
-  Spinner = CLI.Spinner;
-var login_spinner = new Spinner("Logging in...", [
-  "◜",
-  "◠",
-  "◝",
-  "◞",
-  "◡",
-  "◟",
-]);
-var blessed = require("blessed");
-
-// constants
 const { Client } = require("discord.js-selfbot-v13");
+const { convert } = require('node-blessed/lib/colors');
 const client = new Client({ checkUpdate: false });
-const prompt = require("prompt-sync")({ sigint: true });
 
-async function menu() {
-  const guildnames = client.guilds.cache.map((guild) => guild.name);
-  const guildids = client.guilds.cache.map((guild) => guild.id);
-  var guilddict = {};
+async function convertunix(unix) {
+  var date = new Date(unix);
+  var hour = date.getHours().toString();
+  var minute = date.getMinutes().toString();
+  var second = date.getSeconds().toString();
 
-  login_spinner.stop();
-  console.clear();
+  if (hour.length == 1) {
+    hour = "0" + hour;
+  }
 
-  var screen = blessed.screen({
-    smartCSR: true,
-    fullUnicode: true,
-    dockBorders: true,
-  });
+  if (minute.length == 1) {
+    minute = "0" + minute;
+  }
 
-  screen.title = `Logged in as ${client.user.tag}`;
-  console.log(`Server list for ${client.user.tag}:\n`);
+  if (second.length == 1) {
+    second = "0" + second;
+  }
+
+  var formattedTime = `${hour}:${minute}:${second}`;
+  return formattedTime;
+}
+
+client.on("ready", async () => {
+  global.guildnames = client.guilds.cache.map((guild) => guild.name);
+  global.guildids = client.guilds.cache.map((guild) => guild.id);
+  var serverlist_dict = { extended: true, children: {} };
 
   for (i in zip([guildnames, guildids])) {
-    var index = Number(i) + 1;
-    guilddict[index] = guildids[i];
-    console.log(`${index}: ${guildnames[i]}`);
+    serverid = guildids[i];
+    servername = guildnames[i];
+
+    var guild = await client.guilds.fetch(serverid);
+    var channel_names = guild.channels.cache
+      .filter((channel) => channel.type === "GUILD_TEXT")
+      .map((channel) => channel.name);
+    var channel_ids = guild.channels.cache
+      .filter((channel) => channel.type === "GUILD_TEXT")
+      .map((channel) => channel.id);
+
+    serverlist_dict["children"][servername] = {}
+    serverlist_dict["children"][servername]["children"] = {}
+
+    for (j in zip([channel_names, channel_ids])) {
+      var index = j + 1;
+      global.channel = await client.channels.fetch(channel_ids[j]);
+      global.channel_viewable = channel
+        .permissionsFor(client.user)
+        .has("VIEW_CHANNEL");
+      
+      if (channel_viewable) {
+        serverlist_dict["children"][servername]["children"][index] = {
+          name: `#${channel_names[j]}`,
+          myCustomProperty: channel_ids[j],
+        };
+      }
+    }
   }
 
-  console.log(
-    `\nType the number of the server you want to enter, or type "exit" to exit.`
-  );
-  var guildid = prompt("Server number: ");
-
-  if (guildid == "exit") {
-    process.exit(0);
-  } 
-  
-  else if (guildid in guilddict) {
-    await getChannelList(guilddict, guildid);
-  } 
-  
-  else {
-    console.log("Invalid server number.");
-    process.exit(0);
-  }
-}
-
-async function getChannelList(dict, id) {
-  global.guild = await client.guilds.fetch(dict[id]);
-  global.channel_names = guild.channels.cache
-    .filter((channel) => channel.type === "GUILD_TEXT")
-    .map((channel) => channel.name);
-  global.channel_ids = guild.channels.cache
-    .filter((channel) => channel.type === "GUILD_TEXT")
-    .map((channel) => channel.id);
-  global.member_names = guild.members.cache.map(
-    (member) => member.user.username
-  );
-  global.member_ids = guild.members.cache.map((member) => member.user.id);
-  global.channeldict = {};
-
-  console.clear();
-
-  for (i in zip([channel_names, channel_ids])) {
-    var index = Number(i) + 1;
-    channeldict[index] = channel_ids[i];
-  }
-
-  var channelnum = "1";
-  getChannel(channelnum);
-}
-
-async function getChannel(channelnum) {
-  console.clear();
-
-  screen = blessed.screen({
-    smartCSR: true,
-    fullUnicode: true,
-    dockBorders: true,
-  });
-
-  global.ChannelListBox = blessed.log({
+  global.ServerList = contrib.tree({
     top: "top",
     left: "left",
     width: "15%",
     height: "100%",
+    label: " {bold}Server List{/bold} ",
     tags: true,
     border: {
       type: "line",
@@ -112,13 +89,16 @@ async function getChannel(channelnum) {
       border: {
         fg: "white",
       },
+      selected: {
+        fg: "black",
+        bg: "white",
+      },
     },
-    keys: true,
   });
 
   global.MessagesBox = blessed.log({
     left: "15%",
-    width: "71%",
+    width: "85.4%",
     height: "86%",
     tags: true,
     border: {
@@ -132,17 +112,17 @@ async function getChannel(channelnum) {
     },
   });
 
-  global.EnterMessageBox = blessed.textbox({
+  global.EnterMessageBox = blessed.textarea({
     top: "86%",
     left: "15%",
-    width: "71%",
-    height: "17%",
+    width: "85.4%",
+    height: "16%",
     inputOnFocus: true,
     tags: true,
     border: {
       type: "line",
     },
-    label: " Enter Message ",
+    label: " {bold}Enter Message{/bold} ",
     style: {
       fg: "white",
       border: {
@@ -151,203 +131,88 @@ async function getChannel(channelnum) {
     },
   });
 
-  global.MemberList = blessed.log({
-    left: "86%",
-    width: "14.83%",
-    height: "100%",
-    tags: true,
-    border: {
-      type: "line",
-    },
-    label: " Member List ",
-    style: {
-      fg: "white",
-      border: {
-        fg: "white",
-      },
-    },
+  ServerList.on('select', async function(node) {
+    global.channel_id = node.myCustomProperty;
+    if (node.myCustomProperty){
+      global.server_name = node.parent.name;
+      await getMessages(node.myCustomProperty);
+    }
+
+    global.server_name = ' {bold}Server List{/bold} ';
+    screen.render();
+  }),
+
+  ServerList.key(['escape', 'q', 'C-c'], function(ch, key) {
+    return process.exit(0);
   });
 
-  MessagesBox.setLabel(` #${channel_names[channelnum - 1]} `);
-  ChannelListBox.setLabel(` ${guild.name} `);
+  ServerList.setData(JSON.parse(JSON.stringify(serverlist_dict)));
+  ServerList.focus();
 
-  // make ui
-  screen.append(ChannelListBox);
+  screen.append(ServerList);
   screen.append(MessagesBox);
   screen.append(EnterMessageBox);
-  screen.append(MemberList);
 
-  EnterMessageBox.key("enter", function (ch, key) {
-    if (EnterMessageBox.getValue().trim().length != 0) {
-      // message command handler
-      if (EnterMessageBox.getValue() == "*exit") {
-        EnterMessageBox.clearValue();
-        EnterMessageBox.focus();
-        screen.render();
-
-        process.exit(0);
-      } 
-      
-      else if (EnterMessageBox.getValue() == "*menu") {
-        EnterMessageBox.clearValue();
-        EnterMessageBox.focus();
-        screen.render();
-        screen.destroy();
-        menu();
-      } 
-      
-      else if (EnterMessageBox.getValue() == "*clear") {
-        EnterMessageBox.setValue("");
-        MessagesBox.setContent("");
-        EnterMessageBox.focus();
-        screen.render();
-      } 
-      
-      else if (EnterMessageBox.getValue().startsWith("*switchchannel")) {
-        var channel_id = EnterMessageBox.getValue().split(" ")[1];
-        if (channel_id in channeldict) {
-          getChannel(channel_id);
-        } 
-        
-        else {
-          MessagesBox.log("Invalid channel number.");
-          EnterMessageBox.clearValue();
-          EnterMessageBox.focus();
-          screen.render();
-        }
-      }
-
-      // if no command send message
-      else {
-        try {
-          if (channel_viewable == true) {
-            if (channel_sendable == true) {
-              client.channels.cache
-                .get(channeldict[channelnum])
-                .send(EnterMessageBox.value);
-            } 
-            
-            else if (channel_sendable == false) {
-              MessagesBox.log("{bold}{red-fg}Error:{/red-fg}{/bold} You do not have permission to send messages in this channel.");
-            }
-          } 
-          
-          else if (channel_viewable == false) {
-            MessagesBox.log("{bold}{red-fg}Error:{/red-fg}{/bold} You do not have permission to view this channel nor send messages to it.");
-          }
-        } 
-
-        catch (err) {
-          MessagesBox.log(
-            `{bold}{red-fg}Error:{/red-fg}{/bold} ${err}}`
-          );
-        }
-
-        EnterMessageBox.clearValue();
-        EnterMessageBox.focus();
-        screen.render();
-      }
-    } 
-    
-    else {
-      MessagesBox.log(
-        "{bold}{red-fg}Error:{/red-fg}{/bold} You cannot send an empty message."
-    );}
-
-    EnterMessageBox.clearValue();
-    EnterMessageBox.focus();
-    screen.render();
-  });
-
-  // render ui and make message box in focus
-  EnterMessageBox.focus();
   screen.render();
-
-  await getMessages(channeldict, channelnum);
-}
-
-async function getMessages(dict, id) {
-  global.channel = await client.channels.fetch(dict[id]);
-  global.channel_viewable = channel
-    .permissionsFor(client.user)
-    .has("VIEW_CHANNEL");
-  global.channel_sendable = channel
-    .permissionsFor(client.user)
-    .has("SEND_MESSAGES");
-  global.channelid = channel.id;
-
-  try {
-    global.messages = await channel.messages.fetch({ limit: 50 });
-
-    for (const [id, message] of messages.reverse()) {
-      try {
-        const attatchments = message.attachments.map(
-          (attachments) => attachments.url
-        );
-
-        if (attatchments.length != 0) {
-          MessagesBox.log(
-            `${message.author.username}#${message.author.discriminator}: ${message.cleanContent}${attatchments}`
-          );
-        } 
-        
-        else {
-          MessagesBox.log(
-            `${message.author.username}#${message.author.discriminator}: ${message.cleanContent}`
-          );
-        }
-      } 
-      
-      catch {
-        void 0;
-      }
-    }
-  } 
-  
-  catch {
-    MessagesBox.log(
-      "{bold}{red-fg}Error:{/red-fg}{/bold} You don't have access to this channel."
-    );
-  }
-
-  ChannelListBox.log("");
-  for (i in zip([channel_names, channel_ids])) {
-    var index = Number(i) + 1;
-    if (channel_names[i] == channel.name) {
-      ChannelListBox.log(`{bold}${index}. #${channel_names[i]}{/bold}`);
-    } 
-    
-    else {
-      ChannelListBox.log(`${index}. #${channel_names[i]}`);
-    }
-  }
-
-  for (i in zip([member_names, member_ids])) {
-    MemberList.log(`- ${member_names[i]}`);
-  }
-
-  EnterMessageBox.focus();
-}
-
-client.on("ready", async () => {
-  menu();
 });
 
-client.on("messageCreate", (message) => {
-  try {
-    if (message.channel.id === channelid) {
+async function getMessages(id) {
+  global.channel = await client.channels.fetch(id);
+  global.messages = await channel.messages.fetch({ limit: 100 });
+
+  ServerList.setLabel(` {bold}${server_name}{/bold} `);
+  MessagesBox.setLabel(` {bold}#${channel.name}{/bold} `);
+  MessagesBox.setContent("");
+
+  for (const [id, message] of messages.reverse()) {
+    try {
       const attatchments = message.attachments.map(
         (attachments) => attachments.url
       );
+
+      if (attatchments.length != 0) {
+        if (message.cleanContent.length != 0) {
+          MessagesBox.log(
+            `${await convertunix(message.createdTimestamp)} ${message.author.username}#${message.author.discriminator}: ${message.cleanContent}\n${attatchments}`
+          );
+        } 
+        
+        else {
+          MessagesBox.log(
+            `${await convertunix(message.createdTimestamp)} ${message.author.username}#${message.author.discriminator}: ${attatchments}`
+          );
+        }
+      } 
+      
+      else {
+        MessagesBox.log(
+          `${await convertunix(message.createdTimestamp)} ${message.author.username}#${message.author.discriminator}: ${message.cleanContent}`
+        );
+      }
+    } 
+    
+    catch {
+      void 0;
+    }
+  }
+}
+
+client.on("messageCreate", async (message) => {
+  try {
+    if (message.channel.id === channel_id) {
+      const attatchments = message.attachments.map(
+        (attachments) => attachments.url
+      );
+
       if (attatchments.length != 0) {
         MessagesBox.log(
-          `${message.author.username}#${message.author.discriminator}: ${message.cleanContent} ${attatchments}`
+          `${await convertunix(message.createdTimestamp)} ${message.author.username}#${message.author.discriminator}: ${message.cleanContent}\n${attatchments}`
         );
       } 
       
       else {
         MessagesBox.log(
-          `${message.author.username}#${message.author.discriminator}: ${message.cleanContent}`
+          `${await convertunix(message.createdTimestamp)} ${message.author.username}#${message.author.discriminator}: ${message.cleanContent}`
         );
       }
     }
@@ -358,7 +223,8 @@ client.on("messageCreate", (message) => {
   }
 });
 
-console.clear();
-login_spinner.start();
+async function sendMessage(id, message) {
+  client.channels.cache.get(id).send(message);
+}
 
 client.login(config.client.token);
