f9db7d431add50a6fb9a8fafc426496df8b28d3d
13-05 <1305@national.shitposting.agency>
2022-10-25 21:13:41 -0500

    begin refactor; doesn't work yet, but the general changes are clear

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const contrib = require("blessed-contrib");
 const CLI = require("clui"), Spinner = CLI.Spinner;
 
 
+/* <- config parsing -> */
+const config = ini.parse(fs.readFileSync("./config.ini", "utf-8"));
+
+
 /* <- globals, functions -> */
 // client class to improve code structure; may change reference name in the future
 class TermClient {
@@ -24,75 +28,217 @@ class TermClient {
       dockBorders: true,
       autoPadding: true,
     });
+
+    this.screen.title = "Discord Terminal Client";
+  }
+
+  async getMessages(node_name, id) {
+	  const channel = await client.channels.fetch(id);
+	  messages = await channel.messages.fetch({ limit: 100 });
+
+	  if (channel.type == "DM") {
+	  	ServerList.setLabel(` {bold}DMs{/bold} `);
+	  	MessagesBox.setLabel(` {bold}${node_name}{/bold} `);
+  	} else {
+	  	ServerList.setLabel(` {bold}${server_name}{/bold} `);
+	  	MessagesBox.setLabel(` {bold}#${channel.name}{/bold} `);
+	  }
+
+	  MessagesBox.setContent("");
+
+	  for (const [id, message] of messages.reverse()) {
+	  	try {
+	  		const attatchments = message.attachments.map(
+	  			(attachments) => attachments.url
+	  		);
+	  		let time = utils.convertUnix(message.createdTimestamp);
+
+	  		if (attatchments.length != 0) {
+	  			if (message.cleanContent.length != 0) {
+	  				MessagesBox.log(
+	  					`${time} ${message.author.username}#${message.author.discriminator}: ${message.cleanContent}\n${attatchments}`
+	  				);
+		  		} else {
+		  			MessagesBox.log(
+		  				`${time} ${message.author.username}#${message.author.discriminator}: ${attatchments}`
+		  			);
+			  	}
+		  	} else {
+		  		MessagesBox.log(
+		  			`${time} ${message.author.username}#${message.author.discriminator}: ${message.cleanContent}`
+		  		);
+		  	}
+	  	} catch {
+		  	void 0;
+		  }
+  	}
   }
 }
+
+// utils
+utils = {
+  zip: (rows) => rows[0].map((_, c) => rows.map((row) => row[c])),
+
+  checkIsEmpty: (str) => str.trim() === '',
+
+  convertUnix: (timestamp) => {
+    let date = new Date(timestamp);
+    let hour = date.getHours().toString();
+    let minute = date.getMinutes().toString();
+    let second = date.getSeconds().toString();
+
+    hour = hour.length == 1 ? `0${hour}` : hour;
+    minute = minute.length == 1 ? `0${minute}` : minute;
+    second = second.length == 1 ? `0${second}` : second;
+
+    return `${hour}:${minute}:${second}`;
+  },
+
+  writeHelp: (log) => { // `log` is type function; should be a function that accepts a string as its first argument and prints the string, or outputs it in some form.
+    log("{bold}Welcome to Discord Terminal!{/bold}");
+
+	  log(
+	  	`This client was written by https://github.com/paintingofblue in JavaScript. It is still in development, so expect bugs.`
+	  );
+	  log(
+	  	`If you have downloaded this outside of GitHub, you can find the source code here: https://github.com/paintingofblue/discord-terminal-client\n`
+	  );
+
+	  log(
+	  	`To get started, press ${startkey}Tab${endkey} to switch to the message box, use the ${startkey}Arrow Keys${endkey} to navigate & ${startkey}Enter${endkey} to select items in the list.`
+	  );
+    log(
+	  	`Press ${startkey}Tab${endkey} again to switch back to the server list.`
+	  );
+	  log(
+	  	`Press ${startkey}Enter${endkey} to send a message when the message box is focused.`
+	  );
+	  log(`Press ${startkey}Escape${endkey} to exit.`);
+  },
+
+  configureDisplay: (termclient) => { // `termclient` is an instance of `TermClient`
+    termclient.screen.title = `Discord Terminal Client - ${client.user.tag}`
+	  ServerList = contrib.tree({
+	  	top: "top",
+	  	left: "left",
+	  	width: "15%",
+	  	height: "100%",
+	  	label: " {bold}Server List{/bold} ",
+	  	tags: true,
+	  	border: {
+	  		type: "line",
+		  },
+		  style: {
+		  	fg: "white",
+		  	border: {
+		  		fg: "white",
+		  	},
+		  	selected: {
+		  		fg: "black",
+		  		bg: "white",
+		  	},
+	  	},
+	  });
+
+	  MessagesBox = blessed.log({
+	  	left: "15%",
+	  	width: "85.4%",
+	  	height: "86%",
+	  	tags: true,
+	  	border: {
+	  		type: "line",
+	  	},
+	  	style: {
+	  		fg: "white",
+	  		border: {
+	  			fg: "white",
+	  		},
+	  	},
+	  });
+
+	  EnterMessageBox = blessed.textarea({
+	  	top: "86%",
+	  	left: "15%",
+	  	width: "85.4%",
+	  	height: "15%",
+	  	tags: true,
+	  	border: {
+	  		type: "line",
+	  	},
+	  	label: " {bold}Enter Message{/bold} ",
+	  	style: {
+	  		fg: "white",
+	  		border: {
+	  			fg: "white",
+	  		},
+  		},
+	  });
+
+	  ServerList.on("select", async function (node) {
+	  	if (node.myCustomProperty) {
+	  		channel_id = node.myCustomProperty;
+	  		server_name = node.parent.name;
+	  		EnterMessageBox.clearValue();
+	  		await termclient.getMessages(node.name, node.myCustomProperty);
+	  	}
+
+	  	server_name = " {bold}Server List{/bold} ";
+	  	termclient.screen.render();
+	  });
+	  
+    termclient.screen.key(["tab"], function (_ch, _key) {
+      if (focused === 0) {
+        focused = 1;
+	  		EnterMessageBox.focus();
+	  		EnterMessageBox.input();
+	  		termclient.screen.render();
+	  	}
+	  });
+
+	  EnterMessageBox.key(["enter"], async function (_ch, _key) {
+      let time = utils.convertUnix(Date.now());
+
+      if (termclient.channel_id == null) {
+        MessagesBox.log(`${time} {red-fg}{bold}[!]{/bold}{/red-fg} You must select a channel to send a message.`);
+      } else {
+        let isEmpty = utils.checkIsEmpty(EnterMessageBox.getValue());
+
+        if (isEmpty) MessagesBox.log(`${time} {red-fg}{bold}[!]{/bold}{/red-fg} You cannot send an empty message.`);
+        else if (!isEmpty) {
+          await termclient.sendMessage(termclient.channel_id, EnterMessageBox.getValue());
+        }
+      }
+
+		  EnterMessageBox.clearValue();
+		  termclient.screen.render();
+	  });
+
+  	EnterMessageBox.key(["tab"], function (_ch, _key) {
+  		focused = 0;
+  		ServerList.focus();
+		screen.render();
+  	});
+
+  	termclient.screen.key(["escape", "C-c"], function (_ch, _key) {
+  		return process.exit(0);
+  	});
+
+  	ServerList.focus();
+
+  	termclient.screen.append(ServerList);
+  	termclient.screen.append(MessagesBox);
+  	termclient.screen.append(EnterMessageBox);
+  }
+}
+
 // globals
+let termclient = new TermClient();
+
 const countdown = new Spinner("Logging in...", ["-", "\\", "|", "/"]);
-const config = ini.parse(fs.readFileSync("./config.ini", "utf-8"));
 const startkey = "{black-fg}{white-bg}";
 const endkey = "{/black-fg}{/white-bg}";
 
-let screen = blessed.screen({
-  smartCSR: true,
-  fullUnicode: config.client.unicode === "true",
-  dockBorders: true,
-  autoPadding: true,
-});
-
-let focused = 0;
-channel_id = "a"; // don't use `let` here; needs to be in `global` scope
-screen.title = "Discord Terminal Client";
-
 // functions
-function zip(rows) {
-  return rows[0].map((_, c) => rows.map((row) => row[c]));
-}
-
-async function checkIfEmpty(str) {	
-	return str.trim() === "";
-}
-
-async function convertunix(unix) {
-	let date = new Date(unix);
-	let hour = date.getHours().toString();
-	let minute = date.getMinutes().toString();
-	let second = date.getSeconds().toString();
-
-	if (hour.length == 1) {
-		hour = "0" + hour;
-	}
-	if (minute.length == 1) {
-		minute = "0" + minute;
-	}
-	if (second.length == 1) {
-		second = "0" + second;
-	}
-
-	return `${hour}:${minute}:${second}`;
-}
-
-async function printHelp() {
-	MessagesBox.log("{bold}Welcome to Discord Terminal!{/bold}");
-
-	MessagesBox.log(
-		`This client was written by https://github.com/paintingofblue in JavaScript. It is still in development, so expect bugs.`
-	);
-	MessagesBox.log(
-		`If you have downloaded this outside of GitHub, you can find the source code here: https://github.com/paintingofblue/discord-terminal-client\n`
-	);
-
-	MessagesBox.log(
-		`To get started, press ${startkey}Tab${endkey} to switch to the message box, use the ${startkey}Arrow Keys${endkey} to navigate & ${startkey}Enter${endkey} to select items in the list.`
-	);
-	MessagesBox.log(
-		`Press ${startkey}Tab${endkey} again to switch back to the server list.`
-	);
-	MessagesBox.log(
-		`Press ${startkey}Enter${endkey} to send a message when the message box is focused.`
-	);
-	MessagesBox.log(`Press ${startkey}Escape${endkey} to exit.`);
-}
-
 async function getMessages(node_name, id) {
 	const channel = await client.channels.fetch(id);
 	messages = await channel.messages.fetch({ limit: 100 });
@@ -112,7 +258,7 @@ async function getMessages(node_name, id) {
 			const attatchments = message.attachments.map(
 				(attachments) => attachments.url
 			);
-			let time = await convertunix(message.createdTimestamp);
+			let time = utils.convertUnix(message.createdTimestamp);
 
 			if (attatchments.length != 0) {
 				if (message.cleanContent.length != 0) {
@@ -137,10 +283,9 @@ async function getMessages(node_name, id) {
 
 async function sendMessage(id, message) {
 	const channel = await client.channels.fetch(id);
-	// resolves an error i got with dms; basically, you can't check permissions of the client in a dm, so let's not check permissions if we're in a dm
   if (message.startsWith(config.client.prefix)) {
     if (message.startsWith(`${config.client.prefix}help`)) {
-      await printHelp();
+      utils.writeHelp(MessagesBox.log);
     }
 
     if (message.startsWith(`${config.client.prefix}clear`)) {
@@ -172,130 +317,14 @@ async function sendMessage(id, message) {
   }
 }
 
-function configure_display() {
-  screen.title = `Discord Terminal Client - ${client.user.tag}`
-	ServerList = contrib.tree({
-		top: "top",
-		left: "left",
-		width: "15%",
-		height: "100%",
-		label: " {bold}Server List{/bold} ",
-		tags: true,
-		border: {
-			type: "line",
-		},
-		style: {
-			fg: "white",
-			border: {
-				fg: "white",
-			},
-			selected: {
-				fg: "black",
-				bg: "white",
-			},
-		},
-	});
-
-	MessagesBox = blessed.log({
-		left: "15%",
-		width: "85.4%",
-		height: "86%",
-		tags: true,
-		border: {
-			type: "line",
-		},
-		style: {
-			fg: "white",
-			border: {
-				fg: "white",
-			},
-		},
-	});
-
-	EnterMessageBox = blessed.textarea({
-		top: "86%",
-		left: "15%",
-		width: "85.4%",
-		height: "15%",
-		tags: true,
-		border: {
-			type: "line",
-		},
-		label: " {bold}Enter Message{/bold} ",
-		style: {
-			fg: "white",
-			border: {
-				fg: "white",
-			},
-		},
-	});
-
-	ServerList.on("select", async function (node) {
-		if (node.myCustomProperty) {
-			channel_id = node.myCustomProperty;
-			server_name = node.parent.name;
-			EnterMessageBox.clearValue();
-			await getMessages(node.name, node.myCustomProperty);
-		}
-
-		server_name = " {bold}Server List{/bold} ";
-		screen.render();
-	}),
-		screen.key(["tab"], function (_ch, _key) {
-			if (focused === 0) {
-				focused = 1;
-				EnterMessageBox.focus();
-				EnterMessageBox.input();
-				screen.render();
-			}
-		});
-
-	EnterMessageBox.key(["enter"], async function (_ch, _key) {
-		let time = await convertunix(Date.now());
-		if (channel_id != "a") {
-			let message = await checkIfEmpty(EnterMessageBox.getValue());
-			if (!message) {
-				await sendMessage(channel_id, EnterMessageBox.getValue());
-			} else if (message) {
-				MessagesBox.log(
-					`${time} {red-fg}{bold}[!]{/bold}{/red-fg} You cannot send an empty message.`
-				);
-			}
-		} else if (channel_id === "a") {
-			MessagesBox.log(
-				`${time} {red-fg}{bold}[!]{/bold}{/red-fg} You must select a channel to send a message.`
-			);
-		}
-
-		EnterMessageBox.clearValue();
-		screen.render();
-	});
-
-	EnterMessageBox.key(["tab"], function (_ch, _key) {
-		focused = 0;
-		ServerList.focus();
-		screen.render();
-	});
-
-	screen.key(["escape", "C-c"], function (_ch, _key) {
-		return process.exit(0);
-	});
-
-	ServerList.focus();
-
-	screen.append(ServerList);
-	screen.append(MessagesBox);
-	screen.append(EnterMessageBox);
-}
-
 
 /* <- client gateway events -> */
 countdown.start();
 
 client.on("ready", async () => {	
   countdown.stop();
-  configure_display();
-	await printHelp();
+  utils.configureDisplay(termclient);
+	utils.writeHelp(MessagesBox.log);
 	MessagesBox.log(`\nLogged in as ${startkey}${client.user.tag}${endkey}`);
 
 	screen.render();
