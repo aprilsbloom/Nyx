d401b19fe0fa9514c018a6bf058ea284c92fb6b2
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2023-03-22 14:03:33 +1300

    Add comments for client class, and add logger

--- client.js
+++ client.js
@@ -1,6 +1,55 @@
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
+
 class TerminalClient {
     constructor() {
-        console.log("terminalclient");
+        this.log = new Logger();
+    }
+
+    // UI things
+    configureScreen() {
+        console.log("configureScreen");
+    }
+
+    logMessage(message) {
+        console.log("logMessage");
+    }
+
+    prompt(type, message) {
+        console.log("prompt");
+    }
+
+    // Client events (Call this after configureScreen, since it uses screen elements)
+
+    // Handle logging in to discord
+    login() {
+        console.log("login");
+    }
+}
+
+class Logger {
+    constructor() {
+        this.red = '\x1b[31m';
+        this.green = '\x1b[32m';
+        this.yellow = '\x1b[33m';
+        this.bold = '\x1b[1m';
+        this.reset = '\x1b[0m';
+    }
+
+    error(message) {
+        console.log(`${this.red}[-]${this.reset} - ${message}`);
+    }
+
+    success(message) {
+        console.log(`${this.green}[+]${this.reset} - ${message}`);
+    }
+
+    warning(message) {
+        console.log(`${this.yellow}[!]${this.reset} - ${message}`);
+    }
+
+    info(message) {
+        console.log(`${this.bold}[-]${this.reset} - ${message}`);
     }
 }
 
@@ -13,11 +62,7 @@ class Utils {
         |   /          |
         |  /  _______  |
         |              |
-        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾`
-    }
-
-    printIcon() {
-        console.log(this.icon);
+        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾`;
     }
 
     zip(rows) {
---
+++ index.js
@@ -0,0 +1,4 @@
+const TerminalClient = require('terminal-client');
+const client = new TerminalClient();
+
+client.login('token');
